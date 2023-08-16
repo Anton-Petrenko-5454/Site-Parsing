dd1f8eedf6a98e2cf0568f42eeab8099c24b5a56
AntonPetrenko5454 <toha20055454@mail.ru>
2023-08-16 16:36:26 +0300

    19.08.2023

--- 1. BeautifulSoup/using_requests.py
+++ 1. BeautifulSoup/using_requests.py
@@ -11,4 +11,14 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup.prettify())
+all_days=soup.find_all('li', class_='forecast-briefly__day swiper-slide')
+
+for day in all_days:
+    name_day=day.find_next('div', class_='forecast-briefly__name').string
+    number_day = day.find_next('time', class_='time forecast-briefly__date').string
+    weather_day = day.find_next('div', class_='forecast-briefly__condition').string
+    middle_temperature_day = day.find_next('div', class_='temp forecast-briefly__temp forecast-briefly__temp_day').string
+    night_temperature_day = day.find_next('div', class_='temp forecast-briefly__temp forecast-briefly__temp_night').string
+    print(f'{name_day} {number_day} , погода {weather_day} , температура днем достигнет {middle_temperature_day} , а ночью упадет до {night_temperature_day}')
+
+
